efb98b546eb6aad6f679fda4d614134e217df1a3
Vitalii Miagkov <maposia@gmail.com>
2026-03-30 11:35:08 +0300

    fix: minimize the app when click buy

--- src/components/SubscribeCTA/SubscribeCTA.tsx
+++ src/components/SubscribeCTA/SubscribeCTA.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Stack } from '@mantine/core'
+import { miniApp } from '@telegram-apps/sdk-react'
 import classes from '@/app/app.module.css'
 import Lottie from 'lottie-react'
 import noSubAnimate from '@public/assets/anamations/no-sub.json'
@@ -11,13 +12,27 @@ export function SubscribeCta() {
 
     const { appConfig } = useAppConfigStoreInfo()
 
+    const handleBuyClick = () => {
+        window.setTimeout(() => {
+            if (miniApp.close.isAvailable()) {
+                miniApp.close(true)
+            }
+        }, 0)
+    }
+
     return (
         <Stack gap="xl">
             <Box className={classes.animateBox} w={200}>
                 <Lottie animationData={noSubAnimate} loop={true} />
             </Box>
             {appConfig?.buyLink && (
-                <Button component={Link} href={appConfig.buyLink} target="_blank" color="cyan">
+                <Button
+                    component={Link}
+                    href={appConfig.buyLink}
+                    onClick={handleBuyClick}
+                    target="_blank"
+                    color="cyan"
+                >
                     {t('main.page.component.buy')}
                 </Button>
             )}
